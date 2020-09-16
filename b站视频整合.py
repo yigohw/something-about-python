20c4a62590ceff5ae59c303e37b2e46c97c44307
yigohw <yigohw@163.com>
2020-09-16 16:23:58 +0800

    Update b站视频整合.py

--- b站视频整合.py
+++ b站视频整合.py
@@ -1,7 +1,7 @@
 import shutil
-home = input("文件位置")
-id = input("av号")
-n = int(input("集数"))
+home = input("文件位置：")
+id = input("av号：")
+n = int(input("集数："))
 for i in range(n):
     i = str(i+1)
     sub = home + '\\' + i
