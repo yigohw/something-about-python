2840eaa0785ecb9ef8bf4b440e4c62e72f43201f
yigohw <yigohw@163.com>
2020-09-16 16:16:34 +0800

    Add files via upload

---
+++ b站视频整合.py
@@ -0,0 +1,9 @@
+import shutil
+home = input("文件位置")
+id = input("av号")
+n = int(input("集数"))
+for i in range(n):
+    i = str(i+1)
+    sub = home + '\\' + i
+    name = id + '_' + i + '_0.mp4'
+    shutil.move(sub+'\\'+name, home)
